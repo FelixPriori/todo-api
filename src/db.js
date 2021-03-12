b456f41e17d950439a4aa714bac2cbdb286b56a3
felix <felix@webisoft.com>
2021-03-12 15:10:04 -0500

    put data in own file

---
+++ src/db.js
@@ -0,0 +1,39 @@
+import {v4 as uuidv4} from 'uuid'
+
+const userIds = {
+  felix: uuidv4(),
+  kim: uuidv4(),
+  kent: uuidv4(),
+}
+
+const users = [
+  {
+    id: userIds.felix,
+    name: 'Felix',
+    email: 'felix@example.com',
+  },
+  {
+    id: userIds.kim,
+    name: 'Kim',
+    email: 'kim@example.com',
+  },
+  {
+    id: userIds.kent,
+    name: 'Kent',
+    email: 'kent@example.com',
+  },
+]
+
+const tasks = [
+  {id: 1, user: userIds.felix, text: 'build my portfolio', completed: false},
+  {id: 2, user: userIds.kim, text: 'eat tacos', completed: true},
+  {id: 3, user: userIds.kent, text: 'do some acting', completed: false},
+  {id: 4, user: userIds.kim, text: 'find a new job', completed: false},
+]
+
+const db = {
+  users,
+  tasks,
+}
+
+export default db
