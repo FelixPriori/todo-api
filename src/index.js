b3d999b9ea064e42024dbdac31c44e2a12d4ae8b
felix <felix@webisoft.com>
2021-03-13 14:34:01 -0500

    import pubsub, add subscriptions to resolvers

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import {GraphQLServer} from 'graphql-yoga'
+import {GraphQLServer, PubSub} from 'graphql-yoga'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
 import User from './resolvers/User'
 import Task from './resolvers/Task'
+import Subscription from './resolvers/Subscription'
 import db from './db'
+
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Task,
   },
   context: {
     db,
+    pubsub,
   },
 })
 
